f23ff70851d9f8e9b61077eb1fc5ed7ae8c65304
Geraldo <gsneto652@gmail.com>
2019-08-07 22:31:42 -0300

    Adicionando estrutura do back
    - Adicionando estrutura inicial do backend
    - Atualizando .gitignore

---
+++ back/src/Hospital.Domain/Interfaces/IRespository.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Domain.Interfaces
+{
+    public interface IRespository<T> where T :BaseEntity
+    {
+        void Insert(T obj);
+        void Update(T obj);
+        void Remove(Guid id);
+        T selectById(Guid id);
+        IList<T> selectAll();
+    }
+}
