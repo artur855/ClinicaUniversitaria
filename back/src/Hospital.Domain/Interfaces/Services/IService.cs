d3057a8da00a14f690cddc6f32558176760b04c0
Geraldo <geraldo@adxti.com.br>
2019-10-15 17:37:07 -0300

    Atualizando Interface IService

--- back/src/Hospital.Domain/Interfaces/Services/IService.cs
+++ back/src/Hospital.Domain/Interfaces/Services/IService.cs
@@ -3,15 +3,16 @@ using Hospital.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Hospital.Domain.Interfaces.Services
 {
-    public interface IService<T> where T : class
+    public interface IService<T> where T : Entity
     {
-        T Post<V>(T obj) where V : AbstractValidator<T>;
-        T Put<V>(T obj) where V : AbstractValidator<T>;
-        void Delete(int id);
-        T Get(int id);
-        IEnumerable<T> Get();
+        Task<T> SaveAsync<V>(T obj) where V : AbstractValidator<T>;
+        Task<T> UpdateAsync<V>(T obj) where V : AbstractValidator<T>;
+        Task<T> DeleteAsync(int id);
+        Task<IEnumerable<T>> ListAsync();
+        Task<T> FindByIdAsync(int id);
     }
 }
