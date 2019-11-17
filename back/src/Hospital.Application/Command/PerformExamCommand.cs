9b891108d13efae0940b0b255bdc23f3808ab1b5
Geraldo <gsneto652@gmail.com>
2019-11-17 03:13:38 -0300

    Adicionando Command e CustomModelBinder

---
+++ back/src/Hospital.Application/Command/PerformExamCommand.cs
@@ -0,0 +1,17 @@
+﻿using Hospital.Application.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Application.Command
+{
+    [ModelBinder(typeof(JsonWithFilesFormDataModelBinder), Name = "date")]
+    public class PerformExamCommand
+    {
+        public int ExamRequestId { get; set; }
+        public IFormFile Exam { get; set; }
+        public string Date { get; set; }
+    }
+}
