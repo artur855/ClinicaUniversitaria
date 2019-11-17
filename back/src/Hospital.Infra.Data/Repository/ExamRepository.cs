8c2e8e55e8b1eebfa40dd31ebe94c305c1371bf5
Geraldo <gsneto652@gmail.com>
2019-11-17 03:09:52 -0300

    Adicionando ExamRepository

---
+++ back/src/Hospital.Infra.Data/Repository/ExamRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hospital.Domain.Entities;
+using Hospital.Domain.Interfaces.Repositories;
+using Hospital.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Infra.Data.Repository
+{
+    public class ExamRepository : Repository<Exam>, IExamRepository
+    {
+        public ExamRepository(HospitalContext context) : base(context)
+        {
+
+        }
+
+    }
+}
